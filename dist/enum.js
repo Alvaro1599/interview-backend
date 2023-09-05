d73b128c10bb22be8a2be9793d4d37c161fe08ca
Alvaro1599 <alvarocanales1599@gmail.com>
2023-09-05 18:23:33 -0500

    remove git dist folder

--- dist/enum.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppI = void 0;
-var AppI;
-(function (AppI) {
-    AppI["PORT"] = "PORT";
-})(AppI || (exports.AppI = AppI = {}));
